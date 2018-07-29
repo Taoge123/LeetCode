cc475bce8bfce8e8e67c993d14703fd2170013b2
taocheng <taocheng984@gmail.com>
2018-07-29 14:46:13 -0400

    add new method

--- Leetcode/Leetcode 103 Binary Tree Zigzag Level Order Traversal.py
+++ Leetcode/Leetcode 103 Binary Tree Zigzag Level Order Traversal.py
@@ -1,9 +1,12 @@
+import collections
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
 
 class Solution:
     def zigzagLevelOrder(self, root):
@@ -41,6 +44,30 @@ class Solution:
             current = next_level
 
         return result
+
+
+    def zigzagLevelOrder(self, root):
+        queue = collections.deque([root])
+        result = []
+
+        while queue:
+            level = []
+            for node in range(len(queue)):
+                q = queue.popleft()
+
+                if q:
+                    level.append(q.vals)
+                    queue.append(q.left)
+                    queue.append(q.right)
+
+            level = level[::-1] if len(result) % 2 else level
+            if level:
+                result.append(level)
+
+        return result
+
+
+
 
 
 
