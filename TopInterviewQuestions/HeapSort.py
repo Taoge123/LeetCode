7f987871b40cf539f25377da27e6603d8fac0a6d
taocheng <taocheng984@gmail.com>
2018-08-09 20:46:44 -0400

    heapy sort

--- TopInterviewQuestions/HeapSort.py
+++ TopInterviewQuestions/HeapSort.py
@@ -1,21 +1,19 @@
 def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1  # left = 2*i + 1
+    r = 2 * i + 2  # right = 2*i + 2
 
-    largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
-
-    #See ig left child of root exists and if its greater than root
     if l < n and arr[i] < arr[l]:
         largest = l
 
-    #also check right child and see if its greater than root
     if r < n and arr[largest] < arr[r]:
         largest = r
 
     if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
 
         heapify(arr, n, largest)
+
 
 def heapSort(arr):
     n = len(arr)
@@ -23,6 +21,14 @@ def heapSort(arr):
     for i in range(n, -1, -1):
         heapify(arr, n, i)
 
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
 
 
-
+arr = [12, 11, 13, 5, 6, 7]
+heapSort(arr)
+n = len(arr)
+print("Sorted array is")
+for i in range(n):
+    print(arr[i])
