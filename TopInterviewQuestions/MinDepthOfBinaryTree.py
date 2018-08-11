67980183f5f09de4220d845f4b5d0451c4ebc919
taocheng <taocheng984@gmail.com>
2018-08-11 14:54:18 -0400

    min depth binary tree

--- TopInterviewQuestions/MinDepthOfBinaryTree.py
+++ TopInterviewQuestions/MinDepthOfBinaryTree.py
@@ -31,3 +31,44 @@ print(minDepth(root))
 
 
 
+#Method 2 by using BFS
+def minDepth(root):
+
+    if root is None:
+        return 0
+
+    q = []
+
+    #Enqueue root and initialize depth as 1
+    q.append({'node' : root, 'depth' : 1})
+
+    while (len(q) > 0):
+        queueItem = q.pop(0)
+
+        #Get details of the removed item
+        node = queueItem['node']
+        depth = queueItem['depth']
+
+        #If this is the first leaf node seen so far then return its depth as answer
+        if node.left is None and node.right is None:
+            return depth
+
+        #IF left subtree is not None, add it to queue
+        if node.left is not None:
+            q.append({'node' : node.left, 'depth' : depth+1})
+
+        if node.right is not None:
+            q.append({'node' : node.right, 'depth' : depth + 1})
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+print(" -- ")
+print(minDepth(root))
+
+
+
+
