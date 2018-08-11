c64a1e02f893c4bfa3b7595f21620e1ec0aadb5f
taocheng <taocheng984@gmail.com>
2018-08-11 14:19:16 -0400

    insertion sort

---
+++ TopInterviewQuestions/InsertionSort.py
@@ -0,0 +1,26 @@
+def insertionSort(arr):
+
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+
+arr = [12, 11, 12, 5, 6]
+insertionSort(arr)
+
+print("After sorted the array is : ")
+for i in range(len(arr)):
+    print("%d" %arr[i])
+
+
+
+
+
+
+
