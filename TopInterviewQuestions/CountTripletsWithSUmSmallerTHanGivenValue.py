48d1944de1f51769955b808b2aea5bb440c4008c
taocheng <taocheng984@gmail.com>
2018-08-12 13:23:15 -0400

    triplets sum

---
+++ TopInterviewQuestions/CountTripletsWithSUmSmallerTHanGivenValue.py
@@ -0,0 +1,24 @@
+def countTriplets(arr, n, sum):
+
+    ans = 0
+
+    for i in range(0, n-2):
+
+        for j in range(i+1, n-1):
+
+            for k in range(j+1, n):
+
+                if (arr[i] + arr[j] + arr[k] < sum):
+
+                    ans += 1
+
+    return ans
+
+arr = [5, 1, 3, 4, 7]
+n = len(arr)
+sum = 12
+print(countTriplets(arr, n, sum))
+
+
+
+
