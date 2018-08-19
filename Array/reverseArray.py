b18d60cd3aa48cb81780f60c5438e30c8cadc619
taocheng <taocheng984@gmail.com>
2018-08-19 19:46:23 -0400

    reverse method adding recursive function

--- Array/reverseArray.py
+++ Array/reverseArray.py
@@ -7,9 +7,20 @@ def reverse(A, start, end):
         start += 1
         end -= 1
 
+
+def reverseRecursively(A, start, end):
+
+    if start >= end:
+        return
+
+    #Base case
+    A[start], A[end] = A[end], A[start]
+
+    reverseRecursively(A, start + 1, end - 1)
+
 A = [1, 2, 3, 4, 5, 6]
 print(A)
-reverse(A, 0, 5)
+reverseRecursively(A, 0, 5)
 print("Reversed list is")
 print(A)
 
